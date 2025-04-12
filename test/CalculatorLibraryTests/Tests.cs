7f40ff900b5636715a908ddeef3673ce48f3579c
Orima <amirsalimi1394@gmail.com>
2025-04-12 20:29:56 +0330

    change Naming

--- test/CalculatorLibraryTests/Tests.cs
+++ test/CalculatorLibraryTests/Tests.cs
@@ -5,17 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using CalculatorLibrary; // Importing the CalculatorLibrary namespace to access the Calculator class.
 
-namespace CalculatorLibraryTests // Namespace for the test project.
+namespace CalculatorLibrary.Tests.Unit // Namespace for the test project.
 {
-    public class Tests // A class containing test methods for the Calculator class.
+    public class CalculatorTests // A class containing test methods for the Calculator class.
     {
          [Fact] // An attribute indicating that this method is a test method.  
-        public void TestAdd() // A test method to verify the Add method of the Calculator class.
+        public void Add_ShouldAddTwoNumbers_whenTwoNumbersAreIntegers() // A test method to verify the Add method of the Calculator class.
         {
             // Create an instance of the Calculator class.
+            //Arrange
             var calculator = new Calculator();
 
             // Call the Add method with two numbers (5 and 3) and store the result.
+            //Act
             var result = calculator.Add(5, 3);
 
             // Assert that the result is equal to 8. This is a test validation step.
