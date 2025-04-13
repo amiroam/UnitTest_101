16f7f868e2485940acb950d458431d1e6074bac8
Orima <amirsalimi1394@gmail.com>
2025-04-13 20:36:49 +0330

    Add : Skip For Test

--- test/CalculatorLibraryTests/Tests.cs
+++ test/CalculatorLibraryTests/Tests.cs
@@ -11,11 +11,12 @@ namespace CalculatorLibrary.Tests.Unit
     public class CalculatorTests 
     {
         private readonly Calculator _sut = new();  
+     //   [Theory(Skip ="This Breaks In CI")]
         [Theory]
-        [InlineData(5, 3, 8)]
+        [InlineData(5, 3, 8, Skip ="This Breaks In CI")]
         [InlineData(0, 0, 0)]
-        [InlineData(7, 3, 10)]
-        [InlineData(51, 3, 54)]
+        [InlineData(5, 4, 9)]
+        [InlineData(5, 10, 15)]
         // This test method verifies that the Add method in the Calculator class 
         // correctly adds two integer numbers and returns the expected
         // It uses the [Theory] attribute to test multiple input values (a, b) 
@@ -33,6 +34,15 @@ namespace CalculatorLibrary.Tests.Unit
            
         }
 
-      
+        [Fact(Skip ="This IS Just A Test")]
+        public void subtract_ShouldSubtractTwoNumbers_whenTwoNumbersAreIntegers()
+        {
+            //Act
+            var result = _sut.Subtract(10, 2);
+            //Assert
+            Assert.Equal(8, result);
+        }
+
+
     }
 }
