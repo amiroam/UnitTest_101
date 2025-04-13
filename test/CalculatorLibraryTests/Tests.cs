a872a310fe82dd3f52972776d093650dc5c78a36
Orima <amirsalimi1394@gmail.com>
2025-04-13 09:13:22 +0330

    Addd: A private readonly field for the System Under Test

--- test/CalculatorLibraryTests/Tests.cs
+++ test/CalculatorLibraryTests/Tests.cs
@@ -9,16 +9,13 @@ namespace CalculatorLibrary.Tests.Unit // Namespace for the test project.
 {
     public class CalculatorTests // A class containing test methods for the Calculator class.
     {
-         [Fact] // An attribute indicating that this method is a test method.  
+        private readonly Calculator _sut = new (); // A private readonly field for the System Under Test (SUT), which is an instance of the Calculator class.
+        [Fact] // An attribute indicating that this method is a test method.  
         public void Add_ShouldAddTwoNumbers_whenTwoNumbersAreIntegers() // A test method to verify the Add method of the Calculator class.
         {
             // Create an instance of the Calculator class.
-            //Arrange
-            var calculator = new Calculator();
-
-            // Call the Add method with two numbers (5 and 3) and store the result.
             //Act
-            var result = calculator.Add(5, 3);
+            var result = _sut.Add(5, 3);
 
             // Assert that the result is equal to 8. This is a test validation step.
             Assert.Equal(8, result);
